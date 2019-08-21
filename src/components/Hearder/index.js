86568c983e6d1281af9f51d78c44d7983e9a7aad
Nilo César <e_maildonilo@yahoo.com.br>
2019-08-21 11:07:47 -0300

    save

---
+++ src/components/Hearder/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo2.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Hearder() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard"> DASHBOARD </Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Nilo Cesar</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Nilo Cesar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmar de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarder</button>
+    </Container>
+  );
 }
